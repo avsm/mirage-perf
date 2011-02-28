4a3f3188a393e44e0ccfb45bbb3cdef6de0bd92d
Anil Madhavapeddy <anil@recoil.org>
2011-02-28 20:52:17 +0000

    disable dns watchdog as memory usage is stable, and tweak script

--- dns/run.sh
+++ dns/run.sh
@@ -41,23 +41,25 @@ function xen_direct {
   mir-xen deens.xen
   # set up bridge
   sudo brctl addbr deens0 || true
+  sudo brctl setfd deens0 0
+  sudo brctl sethello deens0 0
+  sudo brctl stp deens0 off
   sudo ifconfig deens0 10.0.0.1 netmask 255.255.255.0
   sudo ifconfig deens0 up
   # spawn VM
   cp ../minios-config _build
   cd _build
   sudo xl create minios-config &
-  sleep 20
-  # short run to get the binary hot
-  ../../queryperf -l 2 -s 10.0.0.2 < ../../simple_data
-  # then the recorded run
-  (../../queryperf -l 5 -s 10.0.0.2 < ../../simple_data > ../../xen.log;
+  sleep 5
+  ping -c 3 10.0.0.2
+  (../../queryperf -l 30 -s 10.0.0.2 < ../../simple_data > ../../xen.log;
+   sleep 3;
    sudo xl destroy deens;
    sudo ifconfig deens0 down;
    sudo brctl delbr deens0) &
   sudo xl console deens
 }
 
-unix_socket
-unix_direct
+#unix_socket
+#unix_direct
 xen_direct
