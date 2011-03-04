66329f9d0efd762bace27557cd507b6906cf7867
Richard Mortier <mort@cantab.net>
2011-03-04 11:01:38 +0000

    moved stuff

--- dns/expts/generate.sh
+++ dns/expts/generate.sh
@@ -3,16 +3,16 @@
 RANGE="1 10 100 1000 10000"
 for n in $RANGE ; do
   for f in rawdata.conf format.conf ; do
-    rm -f -- "$n-$f"
-    sed "s/@NHOSTS@/$n/g" $f > $n-$f
+    rm -f -- "$f-$n"
+    sed "s/@NHOSTS@/$n/g" $f > $f-$n
   done
-  for f in $n-rawdata.conf ; do
-    rm -f -- "$n-raw.csv"
+  for f in rawdata-$n.conf ; do
+    rm -f -- "raw-$n.csv"
     ../dlz-perf-tools/dnsDataGen.pl $f
   done
-  for f in $n-format.conf ; do
-    rm -rf -- "$n-named/"
-    mkdir $n-named
+  for f in format-$n.conf ; do
+    rm -rf -- "named-$n/"
+    mkdir named-$n
     ../dlz-perf-tools/dnsCSVDataReader.pl $f
   done
 done
