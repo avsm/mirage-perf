b11b010ce8d1154ff54e805b586589ab43d39cbb
Richard Mortier <mort@cantab.net>
2011-03-05 22:39:56 +0000

    checkpoint, for real

---
+++ dns/clean.sh
@@ -0,0 +1,3 @@
+#!/bin/sh -ex
+
+rm -rf -- data/*-1*
---
+++ dns/generate.sh
@@ -0,0 +1,24 @@
+#!/bin/sh -ex
+
+RANGE=$(cat RANGE)
+
+if [ ! -r "data/en_GB.dic.stripped" ]; then
+  cut -f 1 -d "/" data/en_GB.dic >| data/en_GB.dic.stripped
+fi
+
+for n in $RANGE ; do
+  for f in rawdata.conf format.conf ; do
+    rm -f -- "$f-$n"
+    sed "s/@NHOSTS@/$n/g" $f > data/${f%.conf}-$n.conf
+  done
+  for f in data/rawdata-$n.conf ; do
+    rm -f -- "raw-$n.csv"
+    dlz-perf-tools/dnsDataGen.pl $f
+  done
+  for f in data/format-$n.conf ; do
+    rm -rf -- "named-$n/"
+    mkdir named-$n
+    dlz-perf-tools/dnsCSVDataReader.pl $f
+  done
+done
+
